a084a72daab7743606324b4f6cde6d0e7e2a6f40
Vividh S Viswanatha <vividhsv@gmail.com>
2017-02-21 16:24:24 -0500

    corrected typo

--- api/config.py
+++ api/config.py
@@ -30,8 +30,8 @@ class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(DB_PATH)
     SECRET_KEY = "\xf9'\xe4p(\xa9\x12\x1a!\x94\x8d\x1c\x99l\xc7\xb7e\xc7c\x86\x02MJ\xa0"
 
-    MAIL_SERVER = os.environ('MAIL_SERVER')
+    MAIL_SERVER = os.environ['MAIL_SERVER']
     MAIL_PORT = 587
-    MAIL_USERNAME = os.environ('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ['MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
     MAIL_DEFAULT_SENDER ="noreply@example.com"
